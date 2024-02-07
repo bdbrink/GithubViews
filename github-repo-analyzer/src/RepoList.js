b05164456b1609f2c1396bfe5395ef832b00220e
bdbrink <brendan.brink@outlook.com>
2024-02-07 15:59:24 -0600

    adding repo list script

---
+++ github-repo-analyzer/src/RepoList.js
@@ -0,0 +1,22 @@
+// src/RepoList.js
+
+import React from 'react';
+
+const RepoList = ({ repos }) => {
+  return (
+    <div>
+      <h2>Repositories:</h2>
+      <ul>
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+              {repo.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RepoList;
